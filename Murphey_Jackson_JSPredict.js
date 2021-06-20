c9008af458e9432a5cc1b73efdb6a583f25c0099
JacksonMurphey <86173426+JacksonMurphey@users.noreply.github.com>
2021-06-20 18:23:29 -0500

    Create Murphey_Jackson_JSPredict.js

---
+++ Murphey_Jackson_JSPredict.js
@@ -0,0 +1,6 @@
+// Predict 1: Undefined would be printed to your console
+// Predict 2: I was born in 1980
+// Predict 3: summing numbers! = undefined 
+//            num1 is 10
+//            num2 is 20
+//            30     
