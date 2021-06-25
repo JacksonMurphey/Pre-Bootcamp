5b2f142e5a6bbcf5ebf2d0538b65e7baf5ab663f
JacksonMurphey <86173426+JacksonMurphey@users.noreply.github.com>
2021-06-24 23:52:28 -0500

    Create Murphey_Jackson_JS_Tinkering.js

---
+++ Murphey_Jackson_JS_Tinkering.js
@@ -0,0 +1,6 @@
+var childHeight=1;
+function displayChildIsAbleToRideTheRollerCoaster(childHeight){
+    if (childHeight > 52) {console.log("Get on that ride, Kiddo!");}
+     else{console.log("Sorry kiddo. Maybe next year");}
+     }
+displayChildIsAbleToRideTheRollerCoaster(54);
